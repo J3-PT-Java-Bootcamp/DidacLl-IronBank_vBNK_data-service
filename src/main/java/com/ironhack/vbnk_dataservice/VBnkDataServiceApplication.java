bc6ddb4b0723d5a287bbfea2f78a80bc6777fb46
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 22:44:13 +0200

    configure connexions

--- src/main/java/com/ironhack/vbnk_dataservice/VBnkDataServiceApplication.java
+++ src/main/java/com/ironhack/vbnk_dataservice/VBnkDataServiceApplication.java
@@ -2,7 +2,9 @@ package com.ironhack.vbnk_dataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class VBnkDataServiceApplication {
 
