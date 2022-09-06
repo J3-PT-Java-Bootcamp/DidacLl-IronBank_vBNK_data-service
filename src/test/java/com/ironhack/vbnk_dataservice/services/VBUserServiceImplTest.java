59c4d9cbcd49efa94e6ca5fbf7feae4999a50a29
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-06 22:04:07 +0200

    User Endpoints implemented

---
+++ src/test/java/com/ironhack/vbnk_dataservice/services/VBUserServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.ironhack.vbnk_dataservice.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VBUserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUnknown_test() {
+    }
+
+    @Test
+    void getThirdParty_test() {
+    }
+
+    @Test
+    void getAccountHolder_test() {
+    }
+
+    @Test
+    void getAdmin_test() {
+    }
+
+    @Test
+    void update_test() {
+    }
+
+    @Test
+    void delete_test() {
+    }
+
+    @Test
+    void getAllAccountHolder_test() {
+    }
+
+    @Test
+    void getAllThirdParty_test() {
+    }
+
+    @Test
+    void getAllAdmin_test() {
+    }
+
+    @Test
+    void create_test() {
+    }
+}
